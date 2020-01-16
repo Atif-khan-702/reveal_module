8163681483797194fd59b4da795904968b8845d9
Atif <atifpathan702@gmail.com>
2020-01-16 14:44:35 +0530

    custom commited

--- custom.js
+++ custom.js
@@ -1,25 +1,17 @@
-window.onload=function(){
-    document.getElementById("register").onclick=formModule.insertRecord;
-    document.getElementById("update").onclick=formModule.updateRecord;
-    document.getElementById("country").addEventListener("change", function() {
-      formModule.populate1('country', 'state','city');
-    });
-    document.getElementById("state").addEventListener("change", function() {
-      formModule.populate2('state','city');
-    });
-    document.getElementById('email').onchange=formModule.myFunction1;
-    document.getElementById('color_change').onclick=formModule.myFunction;
-    
-  }
-  arr=['fname','lname','fatherName','motherName','blood','email','mobile','adhar','gender','dob','marry','country','state','city'];
-    arr.forEach(function(elem) {
-      document.getElementById(elem).addEventListener("change", function() {
-        formModule.remove_warn(elem);
-    });
-    document.getElementById("edit").addEventListener("click", function() {
-        formModule.editRow();
-    });
-    document.getElementById("delete").addEventListener("click", function() {
-        formModule.deletecnfg();
-    });
-  });
+document.getElementById("register").onclick=formModule.insertRecord;
+document.getElementById("update").onclick=formModule.updateRecord;
+document.getElementById("country").addEventListener("change", function() {
+    formModule.populate1('country', 'state','city');
+});
+document.getElementById("state").addEventListener("change", function() {
+    formModule.populate2('state','city');
+});
+document.getElementById('email').onchange=formModule.myFunction1;
+document.getElementById('color_change').onclick=formModule.myFunction;
+
+arr=['fname','lname','fatherName','motherName','blood','email','mobile','adhar','gender','dob','marry','country','state','city'];
+arr.forEach(function(elem) {
+    document.getElementById(elem).addEventListener("change", function() {
+    formModule.remove_warn(elem);
+});
+})
